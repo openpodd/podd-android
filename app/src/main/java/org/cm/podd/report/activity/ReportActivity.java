60548e5d7c844c962f511e65d16253c4f0f29080
Polawat Phetra <pphetra@gmail.com>
2014-11-27 13:44:54 +0700

    when up button in action bar of report activity is clicked, show confirm dialog only when report is un submit.

--- app/src/main/java/org/cm/podd/report/activity/ReportActivity.java
+++ app/src/main/java/org/cm/podd/report/activity/ReportActivity.java
@@ -458,22 +458,28 @@ public class ReportActivity extends ActionBarActivity
 
         } else if (id == android.R.id.home) {
             // confirm going back to report list home
-            new AlertDialog.Builder(this).setTitle(R.string.title_confirm_back_report_home)
-                    .setMessage(R.string.message_confirm_back_report_home)
-                    .setPositiveButton(R.string.agree, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            // Save as draft and finish this activity
-                            finishReport(ReportDataInterface.DRAFT_ACTION);
-                        }
-                    })
-                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.cancel();
-                        }
-                    })
-                    .create().show();
+            if (reportSubmit == 1) {
+
+                finishReport(ReportDataInterface.CANCEL_ACTION);
+
+            } else {
+                new AlertDialog.Builder(this).setTitle(R.string.title_confirm_back_report_home)
+                        .setMessage(R.string.message_confirm_back_report_home)
+                        .setPositiveButton(R.string.agree, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Save as draft and finish this activity
+                                finishReport(ReportDataInterface.DRAFT_ACTION);
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        })
+                        .create().show();
+            }
             return true;
         }
         return super.onOptionsItemSelected(item);
