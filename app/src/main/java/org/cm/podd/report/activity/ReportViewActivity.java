49e6b0d99a86a604ad204e948cbd2de6dca7a661
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2015-02-24 16:28:25 +0700

    [step:6.1] add follow-up reports list

--- app/src/main/java/org/cm/podd/report/activity/ReportViewActivity.java
+++ app/src/main/java/org/cm/podd/report/activity/ReportViewActivity.java
@@ -10,7 +10,12 @@ import android.text.Html;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -19,9 +24,14 @@ import org.cm.podd.report.fragment.FeedAdapter;
 import org.cm.podd.report.service.FilterService;
 import org.cm.podd.report.service.ReportService;
 import org.cm.podd.report.util.DateUtil;
+import org.cm.podd.report.util.RequestDataUtil;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by siriwat on 2/23/15.
@@ -42,8 +52,11 @@ public class ReportViewActivity extends ActionBarActivity {
     private TextView createdByView;
     private TextView createdByTelephoneView;
     private TextView createdByProjectTelephoneView;
-    private TextView formDataExplanation;
+    private TextView formDataExplanationView;
+    private ListView followUpListView;
+    private TextView emptyFollowUpListView;
 
+    private FollowUpItemAdapter followUpItemAdapter;
 
     private BroadcastReceiver mReceiver;
 
@@ -63,7 +76,11 @@ public class ReportViewActivity extends ActionBarActivity {
         createdByView = (TextView) findViewById(R.id.report_view_report_created_by);
         createdByTelephoneView = (TextView) findViewById(R.id.report_view_reporter_telephone);
         createdByProjectTelephoneView = (TextView) findViewById(R.id.report_view_reporter_project_telephone);
-        formDataExplanation = (TextView) findViewById(R.id.report_view_form_data_explanation);
+        formDataExplanationView = (TextView) findViewById(R.id.report_view_form_data_explanation);
+        followUpListView = (ListView) findViewById(R.id.report_follow_up_list);
+        followUpListView.setVisibility(View.GONE);
+        emptyFollowUpListView = (TextView) findViewById(R.id.empty_follow_up_list_text);
+        emptyFollowUpListView.setVisibility(View.VISIBLE);
 
         // register receiver.
         mReceiver = new BroadcastReceiver() {
@@ -117,10 +134,77 @@ public class ReportViewActivity extends ActionBarActivity {
             createdByProjectTelephoneView.setText(linkify(projectTelephone, "tel:" + projectTelephone));
             createdByProjectTelephoneView.setMovementMethod(LinkMovementMethod.getInstance());
 
-            formDataExplanation.setText(FeedAdapter.stripHTMLTags(
+            formDataExplanationView.setText(FeedAdapter.stripHTMLTags(
                     report.getString("formDataExplanation")));
+
+            // Add follow up if exists.
+            if (report.getLong("flag") == 5) {
+                ReportService.FollowUpAsyncTask task = new ReportService.FollowUpAsyncTask() {
+                    @Override
+                    protected void onPostExecute(RequestDataUtil.ResponseObject resp) {
+                        try {
+                            JSONArray followUpReports = new JSONArray(resp.getRawData());
+                            ArrayList<String> textList = new ArrayList<String>();
+
+                            for (int i = 0; i != followUpReports.length(); ++i) {
+                                JSONObject item = followUpReports.getJSONObject(i);
+                                textList.add(item.getString("id"));
+
+                                // Append text view to list view.
+                            }
+
+                            followUpItemAdapter = new FollowUpItemAdapter(getContext(),
+                                    R.layout.list_item_follow_up_report, textList);
+                            followUpListView.setAdapter(followUpItemAdapter);
+
+                            followUpListView.setVisibility(View.VISIBLE);
+                            followUpListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                    Intent intent = new Intent(context, ReportViewActivity.class);
+                                    intent.putExtra("id", Long.parseLong(followUpItemAdapter.getItem(position)));
+                                    startActivityForResult(intent, 0);
+                                }
+                            });
+                            emptyFollowUpListView.setVisibility(View.GONE);
+                        } catch (JSONException e) {
+                            Log.e(TAG, "Error parsing JSON data", e);
+                        }
+                    }
+                };
+                task.setContext(getApplicationContext());
+                task.execute(report.getString("id"));
+            }
         } catch (JSONException e) {
             Log.e(TAG, "Error parsing JSON data", e);
         }
+    }
+
+    private class FollowUpItemAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        int resource;
+        List<String> items;
+
+        public FollowUpItemAdapter(Context context, int resource, List<String> items) {
+            super(context, resource, items);
+            this.context = context;
+            this.resource = resource;
+            this.items = items;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View view = inflater.inflate(resource, parent, false);
+
+            TextView textView = (TextView) view.findViewById(android.R.id.text1);
+            textView.setText(
+                    getString(R.string.follow_up_report_item_template)
+                        .replace(":id", getItem(position)));
+
+            return view;
+        }
+
     }
 }
