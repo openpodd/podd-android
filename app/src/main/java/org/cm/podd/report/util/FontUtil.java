46eefab7eb6c1b17c56f2864d89881d08fa5fe4a
Sudarat Yawutthi <sudarat@Opendreams-MacBook-Pro.local>
2015-02-27 16:05:00 +0700

    add override font util

--- app/src/main/java/org/cm/podd/report/util/FontUtil.java
+++ app/src/main/java/org/cm/podd/report/util/FontUtil.java
@@ -1,7 +1,25 @@
 package org.cm.podd.report.util;
 
-/**
- * Created by sudarat on 2/27/15 AD.
- */
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 public class FontUtil {
+    public static void overrideFonts(final Context context, final View v) {
+        try {
+            if (v instanceof ViewGroup) {
+                ViewGroup vg = (ViewGroup) v;
+                for (int i = 0; i < vg.getChildCount(); i++) {
+                    View child = vg.getChildAt(i);
+                    overrideFonts(context, child);
+                }
+            } else if (v instanceof TextView) {
+                Typeface face = StyleUtil.getDefaultTypeface(context.getAssets(), Typeface.NORMAL);
+                ((TextView) v).setTypeface(face);
+            }
+        } catch (Exception e) {
+        }
+    }
 }
